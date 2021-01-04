5719f38679d7770f31931661fbffe2a277be4d02
William Jackson <william@subtlecoolness.com>
2021-01-04 12:56:58 -0600

    Update call signature of jwt.decode

--- clrkio/app.py
+++ clrkio/app.py
@@ -83,7 +83,7 @@ def authorize():
     resp = requests.post(token_endpoint, data=data).json()
     id_token = resp.get('id_token')
     algorithms = discovery_document.get('id_token_signing_alg_values_supported')
-    claim = jwt.decode(id_token, verify=False, algorithms=algorithms)
+    claim = jwt.decode(id_token, options={'verify_signature': False}, algorithms=algorithms)
     flask.session['email'] = claim.get('email')
     target = flask.session.pop('sign-in-target-url', flask.url_for('index'))
     return flask.redirect(target)
